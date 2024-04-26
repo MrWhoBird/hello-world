99be6f942b0679f4d0878c6ccb62575a44da7437
MrWhoBird <hubert.puddle@gmail.com>
2024-04-26 15:21:54 +0200

    prepare to switch docker-server to k8s

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -8,9 +8,3 @@ useradd ansadmin
 usermod -aG docker dockeradmin
 mkdir /opt/docker
 chown -R dockeradmin:dockeradmin /opt/docker
-cd /opt/docker
-cat <<EOT>> ./Dockerfile
-FROM tomcat:latest
-RUN cp -R  /usr/local/tomcat/webapps.dist/*  /usr/local/tomcat/webapps
-COPY ./*.war /usr/local/tomcat/webapps
-EOT
