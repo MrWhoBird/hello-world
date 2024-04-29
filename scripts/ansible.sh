0ca4c0fc2b00635c6d832096bfb429798faa05ab
MrWhoBird <hubert.puddle@gmail.com>
2024-04-29 18:15:00 +0200

    minor changes

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -5,7 +5,6 @@ yum install ansible -y
 yum install docker -y
 systemctl enable docker
 systemctl start docker
-
 usermod -aG docker ansadmin
 mkdir /opt/docker
 chown -R ansadmin:ansadmin /opt/docker
