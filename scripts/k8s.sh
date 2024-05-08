a5a98cff751f1a5aa9e6b7bb52623a1d1953129c
MrWhoBird <hubert.puddle@gmail.com>
2024-05-08 11:16:44 +0200

    final changes

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -1,12 +1,12 @@
+#!/bin/bash
 sudo -i
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.3/2024-04-19/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
-
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-eksctl version
+mkdir /opt/docker
+chown -R ansadmin:ansadmin /opt/docker
